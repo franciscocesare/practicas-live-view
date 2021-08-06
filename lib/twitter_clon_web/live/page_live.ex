ee95a260df6d38a3b633a67210f8b3d7c2dabbe9
franciscocesare <pako_cesare@hotmail.com>
2021-08-06 02:29:39 -0300

    actions for add, edit, delete words but yet isnt a modal like products

--- lib/twitter_clon_web/live/page_live.ex
+++ lib/twitter_clon_web/live/page_live.ex
@@ -1,16 +1,19 @@
 defmodule TwitterClonWeb.PageLive do
   use TwitterClonWeb, :live_view
-
+  
+  alias TwitterClon.ListWords
+  alias TwitterClon.ListWords.Word
+  
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, counters: gen_counters(1), word: "", results: [])}
+    {:ok, assign(socket, counters: gen_counters(1), words: list_words())}
   end
 
   # Events
-  @impl true
-  def handle_event("create", _params, socket) do
-    {:noreply, assign(socket, counters: add_counters(socket.assigns.counters))}
-  end
+  
+  # def handle_event("create", _params, socket) do
+  #   {:noreply, assign(socket, counters: add_counters(socket.assigns.counters))}
+  # end
 
   def handle_event("increment", %{"id" => counter_id}, socket) do
     {:noreply, assign(socket, counters: increment_counters(socket.assigns.counters, counter_id))}
@@ -20,21 +23,7 @@ defmodule TwitterClonWeb.PageLive do
     {:noreply, assign(socket, counters: decrement_counters(socket.assigns.counters, counter_id))}
   end
 
-  def handle_event("add", %{"w" => word}, socket) do
-    if(word != "",
-      do: {:noreply, assign(socket, results: add_words(socket.assigns.results, word))},
-      else:
-        {:noreply,
-         socket
-         |> put_flash(:error, "The word can't be blank")
-         |> assign(results: socket.assigns.results)}
-    )
-  end
-
-  def handle_event("delete_word", %{"value" => word}, socket) do
-    {:noreply, assign(socket, results: delete_words(socket.assigns.results, word))}
-  end
-
+  
   # Functions
   defp gen_counters(n) do
     Enum.to_list(1..n) |> Enum.reduce(%{}, &Map.put(&2, Integer.to_string(&1), 0))
@@ -48,17 +37,13 @@ defmodule TwitterClonWeb.PageLive do
     Map.update(counters, counter_id, 5, &(&1 - 1))
   end
 
-  defp add_counters(counters) do
-    length = counters |> Map.keys() |> length()
-    Map.put_new(counters, Integer.to_string(length + 1), 0)
-  end
+  # defp add_counters(counters) do
+  #   length = counters |> Map.keys() |> length()
+  #   Map.put_new(counters, Integer.to_string(length + 1), 0)
+  # end
+  
+   defp list_words do
+     ListWords.list_words()
+   end
 
-  defp add_words(results, word) do
-    length = results |> length()
-    results |> List.insert_at(length, word)
-  end
-
-  defp delete_words(results, word) do
-    results |> List.delete(word)
-  end
 end
